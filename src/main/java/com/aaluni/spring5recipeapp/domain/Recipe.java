248cb84a0674ce060e69f0edea2c9f9e5af4f64a
Arindam Aluni <aaluni@gmail.com>
2020-04-20 15:25:52 +0100

    Refactoring Pojo and Helper method

--- src/main/java/com/aaluni/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/aaluni/spring5recipeapp/domain/Recipe.java
@@ -111,9 +111,15 @@ public class Recipe {
 	}
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+		notes.setRecipe(this);
 	}
 	public Difficulty getDifficulty() {
 		return difficulty;
+	}
+	public Recipe addIngredient(Ingredient ingredient) {
+		ingredient.setRecipe(this);
+		this.ingredients.add(ingredient);
+		return this;
 	}
 	public void setDifficulty(Difficulty difficulty) {
 		this.difficulty = difficulty;
